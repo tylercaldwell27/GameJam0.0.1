b3fce1d8f1c2c3c83133575e6745dc3afde98122
rhain-011 <36254247+rhain-011@users.noreply.github.com>
2019-01-25 20:27:01 -0500

    nothing

--- GameJam0.0.1/Assets/Scripts/PlayerControllerRepair.cs
+++ GameJam0.0.1/Assets/Scripts/PlayerControllerRepair.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerControllerRepair : MonoBehaviour
 {
+    public float speed = 5.0f;
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveVal = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveVal * speed, rb.velocity.y);
     }
 }
