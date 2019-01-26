8684b2b6fdcfa4e091945e5fca5d2d5c89b32e11
MarcusS919 <marcussim91@hotmail.com>
2019-01-26 00:08:48 -0500

    enemy spawner

---
+++ GameJam0.0.1/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public float speed=5;
+    public float AttackSpeed=5; 
+    private Transform target;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+   
+}
