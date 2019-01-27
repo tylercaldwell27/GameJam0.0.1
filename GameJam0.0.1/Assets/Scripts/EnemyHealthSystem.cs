3c1fbd4bd112b12583d49ba44d48f7ea5d5d339a
rhain-011 <36254247+rhain-011@users.noreply.github.com>
2019-01-26 22:01:48 -0500

    enemy

--- GameJam0.0.1/Assets/Scripts/EnemyHealthSystem.cs
+++ GameJam0.0.1/Assets/Scripts/EnemyHealthSystem.cs
@@ -9,7 +9,31 @@ public class EnemyHealthSystem : MonoBehaviour
     public float attackDmg;
     public float AttackSpeed;
 
+    [SerializeField]
+    private float flashLength;
 
+    private float flashCounter;
+
+    private Renderer renderer;
+    private Color defaultColor;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        defaultColor = renderer.material.GetColor("_Color");
+    }
+
+    void Update()
+    {
+        if (flashCounter > 0.0f)
+        {
+            flashCounter -= Time.deltaTime;
+            if (flashCounter <= 0.0f)
+            {
+                renderer.material.SetColor("_Color", defaultColor);
+            }
+        }
+    }
 
     public void DeductHealth(float deductHealth)
     {
@@ -20,6 +44,10 @@ public class EnemyHealthSystem : MonoBehaviour
 
             EnemyDead();
         }
+
+        health -= attackDmg;
+        flashCounter = flashLength;
+        renderer.material.SetColor("_Color", Color.red);
     }
 
     void EnemyDead()
